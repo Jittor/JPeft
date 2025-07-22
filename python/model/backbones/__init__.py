c898ca791014c0ec083094c4de7dbfbab298ab25
Jittor <59786298+Jittor@users.noreply.github.com>
2025-07-22 19:28:33 +0800

    Add files via upload

---
+++ python/model/backbones/__init__.py
@@ -0,0 +1,21 @@
+from .resnet import *
+from .ssd_vgg import *
+from .res2net import *
+from .re_resnet import ReResNet
+from .lsknet import *
+from .stripnet import *
+
+
+from .peft_fixed_resnet import *
+from .peft_partial1_resnet import *
+from .peft_bitfit_resnet import *
+from .peft_normtuning_resnet import *
+from .peft_adapter_resnet import *
+from .peft_adaptformer_resnet import *
+from .peft_convadapter_resnet import *
+from .peft_mona_resnet import *
+from .peft_lorand_resnet import *
+from .peft_lora_resnet import *
+from .full_resnet import *
+
+__all__ = []
